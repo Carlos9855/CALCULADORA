6cee234c99c6b8940b0cdab5dfa70f8181f2e376
carlos camacho <josecarloscamacho55@gmail.com>
2021-05-02 21:08:50 -0400

    fifth commit

--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe "Calculadora De Cadenas" do
         expect(calcularCadena("5,1")).to eq(6)
     end
 
-    
+    it "Deberia devolver el numero 8 para la cadena 5, 1 y 2" do
+        expect(calcularCadena("5,1,2")).to eq(8)
+    end
+
+    it "Deberia devolver el numero 8 para la cadena 5, 1 y 2" do
+        expect(calcularCadena("5 1,2")).to eq(8)
+    end
+
+     it "Deberia devolver el numero 8 para la cadena 5, 1002 y 2" do
+        expect(calcularCadena("5 1002,2")).to eq(7)
+    end
 end
 
