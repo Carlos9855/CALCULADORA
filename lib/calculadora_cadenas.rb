8a24c36ffa0fc78487ea757d7561fc181f79901f
carlos camacho <josecarloscamacho55@gmail.com>
2021-05-02 21:06:45 -0400

    Second commit

---
+++ lib/calculadora_cadenas.rb
@@ -0,0 +1,12 @@
+def calcularCadena(numCa)
+    if(numCa.split(/\n/)[0].include?"//")
+        delimitadores =((numCa.split(/\n/)[0].gsub"/","").gsub"[","").gsub"]",""
+        arraynums=numCa.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
+    else
+        arraynums=numCa.split(/[,\s]/)
+    end
+    arraynumsInt=arraynums.map{|i| i.to_i if i.to_i<1001}.compact
+    suma =arraynumsInt.sum()
+    return suma
+end
+
---
+++ spec/calculadora_spec.rb
@@ -0,0 +1,13 @@
+require "./lib/calculadora_cadenas.rb"
+RSpec.describe "Calculadora De Cadenas" do
+    it "Deberia devolver el numero 3 para la cadena 3" do
+        expect(calcularCadena("3")).to eq(3)
+    end
+
+    it "Deberia devolver el numero 5 para la cadena 5" do
+        expect(calcularCadena("5")).to eq(5)
+    end
+
+   
+end
+
